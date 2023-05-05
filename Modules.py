e14aa314227963d29e26f36396a293181b9ba82b
Marek Horsky <xhorsky@fi.muni.cz>
2023-05-05 18:23:41 +0200

    Intensive bug fixing

---
+++ Modules.py
@@ -0,0 +1,18 @@
+"""This file is a collection of imports
+needed to make the project work.
+It makes it easier to generate cohesive and
+functional .exe file from the project."""
+
+from tkinter import *
+from tkinter import font, ttk
+
+import tkcalendar
+import hashlib
+
+import harperdb
+import time
+import babel.numbers
+from sys import exit
+
+from datetime import datetime, date
+from typing import List, Dict, Optional, Callable
